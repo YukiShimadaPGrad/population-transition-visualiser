a9217a56a5dd4c29ab7ed58bf058dab6217d9a95
YukiShimadaPGrad <135424992+YukiShimadaPGrad@users.noreply.github.com>
2024-06-06 14:30:05 +0900

    Storybookにアクセシビリティチェックを追加 #7

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,11 @@ import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
   stories: ["../app/**/*.mdx", "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/nextjs",
